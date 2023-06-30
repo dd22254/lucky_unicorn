bbac17c921554ad69486897b9359d7c27a4afd09
dd22254 <129340828+dd22254@users.noreply.github.com>
2023-06-30 13:23:21 +1200

    sand_box.py

---
+++ sand_box.py
@@ -0,0 +1,10 @@
+greeting = "Hello World"
+sides = "*" * 3
+
+greeting = "{} {} {}".format(sides, greeting, sides)
+
+top_bottom = "*" * len(greeting)
+
+print(top_bottom)
+print()
+print(top_bottom)
